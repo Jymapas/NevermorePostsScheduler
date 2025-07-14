ff3879f65df3dc9239d5dc2c7c89223e9848cfc8
Jymapas <jymapas@yandex.ru>
2025-07-14 23:40:31 +0500

    feat(State): add StateService

---
+++ State/StateService.cs
@@ -0,0 +1,33 @@
+﻿namespace NevermorePostsScheduler.State;
+
+internal class StateService
+{
+    private readonly Dictionary<long, PostDraft> _drafts = new();
+    private readonly Dictionary<long, PostState> _states = new();
+
+    public PostState GetState(long adminId)
+    {
+        return _states.GetValueOrDefault(adminId, PostState.None);
+    }
+
+    public void SetState(long adminId, PostState postState)
+    {
+        _states[adminId] = postState;
+    }
+
+    public PostDraft? GetDraft(long adminId)
+    {
+        return _drafts.GetValueOrDefault(adminId);
+    }
+
+    public void SetDraft(long adminId, PostDraft postDraft)
+    {
+        _drafts[adminId] = postDraft;
+    }
+
+    public void Clear(long adminId)
+    {
+        _states.Remove(adminId);
+        _drafts.Remove(adminId);
+    }
+}
