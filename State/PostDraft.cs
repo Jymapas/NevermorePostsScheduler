6619dcc05233ccac0e5148b5fce83d94dac471a9
Jymapas <jymapas@yandex.ru>
2025-07-14 23:31:57 +0500

    feat: add PostDraft class

---
+++ State/PostDraft.cs
@@ -0,0 +1,9 @@
+﻿namespace NevermorePostsScheduler.State;
+
+internal class PostDraft
+{
+    private long AdminId { get; set; }
+    private int MessageId { get; set; }
+    private string ChannelName { get; set; }
+    private DateTime ScheduledTime { get; set; }
+}
